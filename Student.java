566a0027a486401ad07f72419094183851520f2b
Vladimir Stojcheski <stojcheskivladimir@gmail.com>
2020-04-11 11:55:37 +0200

    Course class added and students.csv modified

--- Student.java
+++ Student.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class Student {
 	String index;
@@ -61,5 +62,52 @@ class Student {
 	public boolean hasSignature() {
 		//TODO
 		return labPoints.size()>8;
+	}
+}
+
+class Course {
+	private List<Student> students;
+
+	public Course()
+	{
+		students = new ArrayList<>();
+	}
+
+	public void addStudent(String index, String firstName, String lastName, ArrayList<Integer> labPoints)
+	{
+		Student student = new Student(index, firstName, lastName, labPoints);
+		students.add(student);
+	}
+
+	public void addStudent(Student student)
+	{
+		students.add(student);
+	}
+
+	public void removeStudent(Student student)
+	{
+		students.remove(student);
+	}
+
+	public void printAllStudentsWithSignature()
+	{
+		for(Student s: students)
+		{
+			if(s.hasSignature())
+			{
+				System.out.println(s.getIndex() + "_" + s.getFirstName() + "_" + s.getLastName());
+			}
+		}
+	}
+
+	public void printStudentsWithAtLeast5AveragePoints()
+	{
+		for(Student s: students)
+		{
+			if(s.getAverage() >= 5)
+			{
+				System.out.println(s.getIndex() + "_" + s.getFirstName() + "_" + s.getLastName());
+			}
+		}
 	}
 }
